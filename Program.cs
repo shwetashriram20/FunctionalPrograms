990cbf8a3791c8639862ae76217c733dd6659aca
shwetashriram20 <shwetashriram20@gamil.com>
2022-12-29 19:35:09 +0530

    Massage

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day5
+{
+    public class Progarm
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Solved DayFive Functional Progarm");
+        }
+    }
+}
