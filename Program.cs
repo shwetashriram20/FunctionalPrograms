cf4743041c95ee0e51b5fb56b5737f85e574d45a
shwetashriram20 <shwetashriram20@gamil.com>
2022-12-30 07:57:54 +0530

    adding files

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Day5
+﻿namespace DayFive
 {
     public class Progarm
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Solved DayFive Functional Progarm");
+            //FlipCoin.CalcualatePercentage();
+
+            // EvenorOdd.CheckEvenorOdd();
+
+            //harmonic.HarmonicValue();
+
+            // LargestofNum.LargestNumber();
+
+            //LeapYear.CalculateLeapYear();
+
+            //powerOf2.power();
+
+            //factors.PrimeFactor();
+
+            //RemainderQuantient.QuanRemainder();
+
+            //swap2num.swapNum();
+
+            VowelConsontant.CheckVowelConsontant();
         }
     }
 }
